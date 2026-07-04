5315f73d42b3dc820a892d1e80abb3bbde0e3dce
karitham <kar@karitham.dev>
2026-07-04 11:21:24 +0200

    apps/hermes: try it out

--- flake.nix
+++ flake.nix
@@ -1,7 +1,14 @@
 {
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
   outputs =
-    { self, nixpkgs }:
+    { self
+    , nixpkgs
+    , treefmt-nix
+    ,
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -13,9 +20,30 @@
         f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      devShells = forEachSupportedSystem (
+      formatter = forEachSupportedSystem (
         { pkgs }:
-        {
+        treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          programs.yamlfmt.enable = true;
+          programs.prettier.enable = true;
+          programs.prettier.settings = { };
+          settings.global.excludes = [
+            "*.sops.yaml"
+            "*secret.yaml"
+            "apps/waifubot/pg.yaml"
+            "clusters/riko/apps/alloy/secret/*"
+            "clusters/riko/core/cert-manager/issuers/secret.yaml"
+            "clusters/riko/flux-system/gotk-components.yaml"
+            "result*"
+            ".jj/**"
+            ".direnv/**"
+          ];
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs }: {
           default = pkgs.mkShell {
             packages =
               let
